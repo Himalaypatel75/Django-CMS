67e14527217b25b791b99ba0d942dffb755a5f55
himalay patel <himalaypatel75@gmail.com>
2023-09-27 20:51:14 +0530

    boiler code setup

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-s2b+(ez5&!u4k-$h%5#cnm(as^!)1+d=x3mx^%1-cuy$10mp_o"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
